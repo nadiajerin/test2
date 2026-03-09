3a2c8df4b5a02261fdc3f3b2ab5feec445144c86
Ahiya9692 <ahiyafiverr@gmail.com>
2026-03-10 00:00:22 +0600

    Update infoData.js

--- app/Data/infoData.js
+++ app/Data/infoData.js
@@ -1,19 +1,20 @@
 export const infoData = [
     // Main First News
+
     {
-            id: 1,
-            image: "https://media.prothomalo.com/prothomalo-bangla%2F2022-05%2F7b4067dd-e102-49e8-a4e0-118f2008f87c%2FMinistry_of_Finance.jpg?rect=70%2C0%2C845%2C563&w=622&auto=format%2Ccompress&fmt=avif",
-            image2: "",
-            image3: "",
-            title: "অর্থ মন্ত্রণালয়ের অধীনে ২০ জনের চাকরির সুযোগ, আবেদন শুরু ১১ মার্চ",
-            desc1: "চাকরি-বাকরি প্রতিবেদক",
-            desc2: "অর্থ মন্ত্রণালয়ের অধীনে নিরীক্ষা, গোয়েন্দা ও তদন্ত অধিদপ্তর, মূল্য সংযোজন কর-এ জনবল নিয়োগের বিজ্ঞপ্তি প্রকাশ করা হয়েছে। প্রতিষ্ঠানটি ১৩ থেকে ১৮তম গ্রেডে মোট ২০ জনকে নিয়োগ দেবে। আগ্রহী প্রার্থীরা ১১ মার্চ থেকে অনলাইনে আবেদন করতে পারবেন।",
-            desc3: "১ থেকে ৫ নম্বর পদের জন্য ১১২ টাকা এবং ৬ ও ৭ নম্বর পদের জন্য ৫৬ টাকা টেলিটক প্রিপেইড সিমের মাধ্যমে জমা দিতে হবে। অনগ্রসর নাগরিক (ক্ষুদ্র জাতিগোষ্ঠী, শারীরিক প্রতিবন্ধী ও তৃতীয় লিঙ্গ) প্রার্থীদের জন্য সব পদে আবেদন ফি ৫৬ টাকা।",
-            desc4: "আবেদন শুরু: ১১ মার্চ ২০২৬, সকাল ১০টা।",
-            desc5: "আবেদনের শেষ সময়: ৮ এপ্রিল ২০২৬, বিকেল ৫টা।",
-            desc6: "",
-            date: " "
-        },
+        id: 17501,
+        image: "https://www.sunamganjerkhobor.com/images/news/6/5723.jpg?newst=1704333360",
+        image2: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhOpxr4WxcLiURE9xti17_Hr5Eb-LGTHSghMKzZXYVWob2JYrILx8JIwiT_HXPWC5GHhNmpUmnzf2_oGXGWtRbQ_WqnrJtVFMu_fgGHo1PjeLwuycN5x-5w3BLeVJEIj8jngiU61SNZhFuqt6HSHxrjAG_osmDyzKCGav609-0l0fQM4iJ-x_6ORHtrmbk/s16000/%E0%A6%B8%E0%A6%BE%E0%A6%AA%E0%A7%8D%E0%A6%A4%E0%A6%BE%E0%A6%B9%E0%A6%BF%E0%A6%95%20%E0%A6%9A%E0%A6%BE%E0%A6%95%E0%A6%B0%E0%A6%BF%E0%A6%B0%20%E0%A6%96%E0%A6%AC%E0%A6%B0%20%E0%A6%AA%E0%A6%A4%E0%A7%8D%E0%A6%B0%E0%A6%BF%E0%A6%95%E0%A6%BE%20%E0%A7%A6%E0%A7%AC%20%E0%A6%AE%E0%A6%BE%E0%A6%B0%E0%A7%8D%E0%A6%9A%20%E0%A7%A8%E0%A7%A6%E0%A7%A8%E0%A7%AC%20%E0%A6%87%E0%A6%82%20%E0%A6%87%E0%A6%AE%E0%A7%87%E0%A6%9C%20%E0%A7%A6%E0%A7%A7.webp",
+        image3: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgVwZ_c0Brpzs_3PZ4MUE8opSEIxHV0QBvjPyvfquZbxW2b18DzMdow4nDR6B44h_exz7tbFQ9TE-vHUEagzp_lpSsoNdvSe4clFbkuMX5NVysOw3Clu1ibiXHM6a9QJulu3QYvaPCFtv_IZP5MRTxUg8JR0A7y1Sk0tiKOybPJVx4VtffMO7boB6AHtqI/s16000/%E0%A6%B8%E0%A6%BE%E0%A6%AA%E0%A7%8D%E0%A6%A4%E0%A6%BE%E0%A6%B9%E0%A6%BF%E0%A6%95%20%E0%A6%9A%E0%A6%BE%E0%A6%95%E0%A6%B0%E0%A6%BF%E0%A6%B0%20%E0%A6%96%E0%A6%AC%E0%A6%B0%20%E0%A6%AA%E0%A6%A4%E0%A7%8D%E0%A6%B0%E0%A6%BF%E0%A6%95%E0%A6%BE%20%E0%A7%A6%E0%A7%AC%20%E0%A6%AE%E0%A6%BE%E0%A6%B0%E0%A7%8D%E0%A6%9A%20%E0%A7%A8%E0%A7%A6%E0%A7%A8%E0%A7%AC%20%E0%A6%87%E0%A6%82%20%E0%A6%87%E0%A6%AE%E0%A7%87%E0%A6%9C%20%E0%A7%A6%E0%A7%A8.webp",
+        title: "সাপ্তাহিক চাকরির খবর পত্রিকা ০৬ মার্চ ২০২৬ ইং",
+        desc1: "সাপ্তাহিক চাকরির খবর ২০২৬",
+        desc2: "",
+        desc3: "",
+        desc4: "",
+        desc5: "",
+        desc6: "",
+        date: " "
+    },
 
     // Bottom All News
 
